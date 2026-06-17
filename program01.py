8b30b9ab182abd27cdb3e989b9678f4bbfb9a7dc
Supanai000 <467084@wsk.ac.th>
2026-06-17 03:17:16 +0000

    program01.py

---
+++ program01.py
@@ -0,0 +1,7 @@
+print("โปรแกรมคำนวนพื้นที่สามเหลี่ยม")
+print("................")
+tall = float(input("enter hieght:"))
+base = float(input("enter base"))
+Areatri =0.1*tall*base
+print("พื้นที่ของสามเหลี่ยม =:,Areatri")
+print("ผุ้พัฒนาโปรแกรม โดย สุภนัย")
